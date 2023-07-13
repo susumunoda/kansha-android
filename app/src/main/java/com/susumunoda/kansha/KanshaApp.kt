7ad04125537d97657cfa036c40f66eb7f168bc66
Susumu Noda <susumunoda@gmail.com>
2023-07-13 14:59:20 -0700

    Implement edge-to-edge UI

--- app/src/main/java/com/susumunoda/kansha/KanshaApp.kt
+++ app/src/main/java/com/susumunoda/kansha/KanshaApp.kt
@@ -1,19 +1,26 @@
 package com.susumunoda.kansha
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -39,21 +46,42 @@ fun KanshaApp(messages: List<Message> = DataSource.allMessages()) {
                 Text("🙏", fontSize = 30.sp)
             }
         },
-        topBar = {}
+        topBar = {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(MaterialTheme.colorScheme.primary)
+                    .windowInsetsPadding(WindowInsets.statusBars)
+            ) {
+                Text("test")
+            }
+        }
     ) { contentPadding ->
-        MessageList(messages, modifier = Modifier.padding(contentPadding))
+        MessageList(
+            messages,
+            modifier = Modifier.padding(
+                top = contentPadding.calculateTopPadding(),
+                start = 16.dp,
+                end = 16.dp
+            )
+        )
     }
 }
 
 @Composable
 private fun MessageList(messages: List<Message>, modifier: Modifier = Modifier) {
     Column(modifier = modifier) {
-        LazyColumn(
-            verticalArrangement = Arrangement.spacedBy(16.dp),
-            modifier = Modifier.padding(16.dp)
-        ) {
+        LazyColumn(verticalArrangement = Arrangement.spacedBy(16.dp)) {
+            item {
+                // Empty spacer to provide even padding before list content
+                Spacer(Modifier)
+            }
             items(messages) { message ->
                 MessageCard(message)
+            }
+            item {
+                // Empty spacer to provide even padding after list content
+                Spacer(Modifier)
             }
         }
     }
--- app/src/main/java/com/susumunoda/kansha/MainActivity.kt
+++ app/src/main/java/com/susumunoda/kansha/MainActivity.kt
@@ -7,11 +7,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.view.WindowCompat
 import com.susumunoda.kansha.ui.theme.KanshaTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             KanshaTheme {
                 // A surface container using the 'background' color from the theme
