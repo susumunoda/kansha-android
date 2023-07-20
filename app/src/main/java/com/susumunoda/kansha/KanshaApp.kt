561715eb67df0642f9e1c7b2c48f6b234a570e21
Susumu Noda <susumunoda@gmail.com>
2023-07-20 13:42:22 -0700

    Add navigation

--- app/src/main/java/com/susumunoda/kansha/KanshaApp.kt
+++ app/src/main/java/com/susumunoda/kansha/KanshaApp.kt
@@ -2,11 +2,23 @@ package com.susumunoda.kansha
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.susumunoda.kansha.ui.screen.listview.MessagesListView
 
+enum class Screen {
+    LIST_VIEW
+}
+
 @Composable
-fun KanshaApp() {
-    MessagesListView()
+fun KanshaApp(navController: NavHostController = rememberNavController()) {
+    NavHost(navController = navController, startDestination = Screen.LIST_VIEW.name) {
+        composable(Screen.LIST_VIEW.name) {
+            MessagesListView()
+        }
+    }
 }
 
 @Preview
