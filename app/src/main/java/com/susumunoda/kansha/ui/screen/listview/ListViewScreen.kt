adea3963948741c1579ff4eb8c18f4b352a12d37
Susumu Noda <susumunoda@gmail.com>
2023-07-21 16:39:53 -0700

    Reimplement filter popup using DropdownMenu
    Solves all of the dismissal issues I was having with Popup, not to
    mention with a much simpler API

--- app/src/main/java/com/susumunoda/kansha/ui/screen/listview/ListViewScreen.kt
+++ app/src/main/java/com/susumunoda/kansha/ui/screen/listview/ListViewScreen.kt
@@ -1,12 +1,9 @@
 package com.susumunoda.kansha.ui.screen.listview
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,7 +11,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
@@ -22,7 +18,8 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Check
 import androidx.compose.material3.Card
 import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.Divider
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -30,7 +27,6 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
@@ -39,14 +35,12 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.window.Popup
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import com.susumunoda.kansha.R
@@ -75,18 +69,13 @@ fun ListViewScreen(
             CenterAlignedTopAppBar(
                 title = { Text("Kansha", color = MaterialTheme.colorScheme.onPrimary) },
                 actions = {
-                    IconButton(
-                        onClick = { isFiltersPopupVisible = !isFiltersPopupVisible },
-                        colors = IconButtonDefaults.iconButtonColors(
-                            contentColor = MaterialTheme.colorScheme.onPrimary
-                        )
-                    ) {
-                        Icon(
-                            painter = painterResource(R.drawable.filter_list),
-                            contentDescription = "Filter list",
-                            modifier = Modifier.size(24.dp)
-                        )
-                    }
+                    FilterAction(
+                        onClick = { isFiltersPopupVisible = true },
+                        onDismiss = { isFiltersPopupVisible = false },
+                        isExpanded = isFiltersPopupVisible,
+                        listViewViewModel = listViewViewModel,
+                        uiState = uiState
+                    )
                 },
                 colors = TopAppBarDefaults.topAppBarColors(
                     containerColor = MaterialTheme.colorScheme.primary
@@ -95,52 +84,54 @@ fun ListViewScreen(
         }
     ) { contentPadding ->
         Column(modifier = Modifier.padding(top = contentPadding.calculateTopPadding())) {
-            if (isFiltersPopupVisible) {
-                Popup(alignment = Alignment.TopEnd) {
-                    Surface(shadowElevation = 5.dp) {
-                        Box(modifier = Modifier.background(MaterialTheme.colorScheme.background)) {
-                            // Force the column to take up the least amount of width necessary
-                            Column(modifier = Modifier.width(IntrinsicSize.Min)) {
-                                ListViewViewModel.FilterType.values()
-                                    .forEachIndexed { index, filterType ->
-                                        if (index != 0) {
-                                            Divider(thickness = 1.dp)
-                                        }
-                                        Row(
-                                            verticalAlignment = Alignment.CenterVertically,
-                                            modifier = Modifier
-                                                // Make area clickable before padding
-                                                .clickable { listViewViewModel.setFilter(filterType) }
-                                                .fillMaxWidth()
-                                                .padding(16.dp)
-                                        ) {
-                                            Box(modifier = Modifier.size(24.dp)) {
-                                                if (uiState.filterType == filterType) {
-                                                    Icon(
-                                                        imageVector = Icons.Rounded.Check,
-                                                        contentDescription = "Filtered by ${filterType.label}",
-                                                        modifier = Modifier.fillMaxSize()
-                                                    )
-                                                }
-                                            }
-                                            Spacer(Modifier.size(8.dp))
-                                            Text(
-                                                text = filterType.label,
-                                                fontSize = 18.sp,
-                                                // Force text to take up max width without wrapping
-                                                modifier = Modifier.width(IntrinsicSize.Max)
-                                            )
-                                        }
-                                    }
-                            }
-                        }
-                    }
-                }
-            }
             MessagesList(
                 messages = uiState.entries,
                 modifier = Modifier.padding(start = 8.dp, end = 8.dp)
             )
+        }
+    }
+}
+
+@Composable
+private fun FilterAction(
+    onClick: () -> Unit,
+    onDismiss: () -> Unit,
+    isExpanded: Boolean,
+    listViewViewModel: ListViewViewModel,
+    uiState: ListViewState,
+    modifier: Modifier = Modifier
+) {
+    Box(modifier = modifier) {
+        IconButton(
+            onClick = onClick,
+            colors = IconButtonDefaults.iconButtonColors(
+                contentColor = MaterialTheme.colorScheme.onPrimary
+            )
+        ) {
+            Icon(
+                painter = painterResource(R.drawable.filter_list),
+                contentDescription = "Filter list",
+                modifier = Modifier.size(24.dp)
+            )
+        }
+        DropdownMenu(
+            expanded = isExpanded,
+            onDismissRequest = onDismiss
+        ) {
+            ListViewViewModel.FilterType.values().forEach { filterType ->
+                DropdownMenuItem(
+                    text = { Text(filterType.label, fontSize = 16.sp) },
+                    onClick = { listViewViewModel.setFilter(filterType) },
+                    leadingIcon = {
+                        if (filterType == uiState.filterType) {
+                            Icon(
+                                imageVector = Icons.Rounded.Check,
+                                contentDescription = "Filtered by ${filterType.label}"
+                            )
+                        }
+                    }
+                )
+            }
         }
     }
 }
