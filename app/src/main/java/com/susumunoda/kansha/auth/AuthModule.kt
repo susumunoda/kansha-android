ca317becdf6c5e455cc6b87f06685dccbe31e407
Susumu Noda <susumunoda@gmail.com>
2023-08-09 17:48:24 -0700

    Install auth Hilt module in SingletonComponent
    Realized that by installing in ActivityComponent, it would not be
    available to ViewModels. Per the removed comment, SingletonComponent was
    probably the correct place to install this anyway.

--- app/src/main/java/com/susumunoda/kansha/auth/AuthModule.kt
+++ app/src/main/java/com/susumunoda/kansha/auth/AuthModule.kt
@@ -3,19 +3,16 @@ package com.susumunoda.kansha.auth
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ActivityComponent
-import dagger.hilt.android.scopes.ActivityScoped
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
-// For now, we can install auth bindings in ActivityComponent because the app uses a single-activity
-// architecture. If in the future the app grows to use multiple activities, this should probably
-// become SingletonComponent so that there is a single source of truth for auth across the entire app.
-@InstallIn(ActivityComponent::class)
+@InstallIn(SingletonComponent::class)
 abstract class AuthModule {
-    // Scope this to the Activity so that all uses of AuthController refer to the same instance.
-    // This is important because the application subscribes to changes in the auth state (e.g. login
-    // and logout events), and all clients must be observing the same source of information.
-    @ActivityScoped
+    // Scope this as a singleton so that all uses of AuthController refer to the same instance.
+    // This is important because the application subscribes to changes in the auth state (e.g.
+    // login and logout events), and all clients must be observing the same source of information.
+    @Singleton
     @Binds
     abstract fun bindAuthController(authController: FirebaseAuthController): AuthController
 }
