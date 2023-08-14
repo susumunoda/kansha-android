b274b0ff86cee8dcc1855bd0154634eb7ee294f5
Susumu Noda <susumunoda@gmail.com>
2023-08-14 16:37:55 -0700

    Fix status bar colors in dark and light modes

--- app/src/main/java/com/susumunoda/kansha/ui/theme/Theme.kt
+++ app/src/main/java/com/susumunoda/kansha/ui/theme/Theme.kt
@@ -1,5 +1,6 @@
 package com.susumunoda.kansha.ui.theme
 
+import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -8,7 +9,10 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -47,6 +51,17 @@ fun KanshaTheme(
 
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
+    }
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            // This is the opposite of what the documentation for WindowInsetsControllerCompat#setAppearanceLightStatusBars
+            // states, but it has the desired outcome — dark foreground color in light mode, light
+            // foreground color in dark mode. The discrepancy may be due to the special handling of
+            // the status bar colors for implementing edge-to-edge UI.
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
+        }
     }
 
     MaterialTheme(
