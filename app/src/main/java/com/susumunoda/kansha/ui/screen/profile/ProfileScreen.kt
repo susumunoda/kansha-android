ff1821734517df5d2d61797f3eb1e6674b54fc9e
Susumu Noda <susumunoda@gmail.com>
2023-08-15 15:11:16 -0700

    Reintroduce Scaffold in ProfileScreen
    • Add top bar (title text, menu icon)
    • Add FAB for note creation

--- app/src/main/java/com/susumunoda/kansha/ui/screen/profile/ProfileScreen.kt
+++ app/src/main/java/com/susumunoda/kansha/ui/screen/profile/ProfileScreen.kt
@@ -4,26 +4,28 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.slideInVertically
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.statusBars
-import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Create
 import androidx.compose.material.icons.rounded.ExitToApp
 import androidx.compose.material.icons.rounded.Menu
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationDrawerItem
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
@@ -51,6 +53,7 @@ import com.susumunoda.kansha.ui.component.DefaultUserPhoto
 import com.susumunoda.kansha.ui.component.UserPhoto
 import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ProfileScreen(viewModel: ProfileScreenViewModel = hiltViewModel()) {
     val uiState by viewModel.uiState.collectAsState()
@@ -79,20 +82,32 @@ fun ProfileScreen(viewModel: ProfileScreenViewModel = hiltViewModel()) {
                 }
             }
         ) {
-            Surface {
+            Scaffold(
+                topBar = {
+                    CenterAlignedTopAppBar(
+                        title = { Text(stringResource(R.string.profile_screen_top_bar)) },
+                        navigationIcon = {
+                            IconButton(onClick = { scope.launch { drawerState.open() } }) {
+                                Icon(Icons.Rounded.Menu, stringResource(R.string.open_menu_drawer))
+                            }
+                        }
+                    )
+                },
+                floatingActionButton = {
+                    FloatingActionButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            Icons.Rounded.Create,
+                            stringResource(R.string.create_note_description)
+                        )
+                    }
+                }
+            ) { contentPadding ->
                 Column(
                     modifier = Modifier
                         .fillMaxSize()
-                        .windowInsetsPadding(WindowInsets.statusBars),
+                        .padding(top = contentPadding.calculateTopPadding()),
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    IconButton(
-                        onClick = { scope.launch { drawerState.open() } },
-                        modifier = Modifier.align(Alignment.Start)
-                    ) {
-                        Icon(Icons.Rounded.Menu, stringResource(R.string.open_menu_drawer))
-                    }
-
                     PhotosSection(
                         backgroundPhotoUrl = uiState.userData.backgroundPhotoUrl,
                         profilePhotoUrl = uiState.userData.profilePhotoUrl
