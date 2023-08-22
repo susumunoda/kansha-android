05fb6211f97bd4f045cd38bc0d11843de88dc4fc
Susumu Noda <susumunoda@gmail.com>
2023-08-22 15:16:49 -0700

    Add 2 new transitions + additional params

--- app/src/main/java/com/susumunoda/kansha/ui/animation/Transitions.kt
+++ app/src/main/java/com/susumunoda/kansha/ui/animation/Transitions.kt
@@ -7,20 +7,54 @@ import androidx.navigation.NavBackStackEntry
 
 private const val SLIDE_DURATION_MILLIS = 350
 
-fun AnimatedContentTransitionScope<NavBackStackEntry>.enterSlidingUp(durationMillis: Int = SLIDE_DURATION_MILLIS) =
+fun AnimatedContentTransitionScope<NavBackStackEntry>.enterSlidingUp(
+    durationMillis: Int = SLIDE_DURATION_MILLIS,
+    targetOffset: (Int) -> Int = { it }
+) =
     slideIntoContainer(
         towards = AnimatedContentTransitionScope.SlideDirection.Up,
         animationSpec = tween(
             durationMillis = durationMillis,
             easing = FastOutSlowInEasing
-        )
+        ),
+        targetOffset
     )
 
-fun AnimatedContentTransitionScope<NavBackStackEntry>.exitSlidingDown(durationMillis: Int = SLIDE_DURATION_MILLIS) =
+fun AnimatedContentTransitionScope<NavBackStackEntry>.exitSlidingDown(
+    durationMillis: Int = SLIDE_DURATION_MILLIS,
+    targetOffset: (Int) -> Int = { it }
+) =
     slideOutOfContainer(
         towards = AnimatedContentTransitionScope.SlideDirection.Down,
         animationSpec = tween(
             durationMillis = durationMillis,
             easing = FastOutSlowInEasing
-        )
+        ),
+        targetOffset
+    )
+
+fun AnimatedContentTransitionScope<NavBackStackEntry>.enterSlidingLeft(
+    durationMillis: Int = SLIDE_DURATION_MILLIS,
+    targetOffset: (Int) -> Int = { it }
+) =
+    slideIntoContainer(
+        towards = AnimatedContentTransitionScope.SlideDirection.Left,
+        animationSpec = tween(
+            durationMillis = durationMillis,
+            easing = FastOutSlowInEasing
+        ),
+        targetOffset
+    )
+
+fun AnimatedContentTransitionScope<NavBackStackEntry>.exitSlidingRight(
+    durationMillis: Int = SLIDE_DURATION_MILLIS,
+    targetOffset: (Int) -> Int = { it }
+) =
+    slideOutOfContainer(
+        towards = AnimatedContentTransitionScope.SlideDirection.Right,
+        animationSpec = tween(
+            durationMillis = durationMillis,
+            easing = FastOutSlowInEasing
+        ),
+        targetOffset
     )
