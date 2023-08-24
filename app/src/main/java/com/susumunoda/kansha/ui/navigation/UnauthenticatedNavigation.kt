9630d6efb11c33435cd87450a0dd9b4cdd092244
Susumu Noda <susumunoda@gmail.com>
2023-08-24 13:49:35 -0700

    Use helper methods for login screen transitions

--- app/src/main/java/com/susumunoda/kansha/ui/navigation/UnauthenticatedNavigation.kt
+++ app/src/main/java/com/susumunoda/kansha/ui/navigation/UnauthenticatedNavigation.kt
@@ -1,20 +1,17 @@
 package com.susumunoda.kansha.ui.navigation
 
-import androidx.compose.animation.AnimatedContentTransitionScope
-import androidx.compose.animation.core.FastOutSlowInEasing
-import androidx.compose.animation.core.tween
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.susumunoda.kansha.ui.animation.enterSlidingLeft
+import com.susumunoda.kansha.ui.animation.exitSlidingRight
 import com.susumunoda.kansha.ui.screen.auth.LoginScreen
 import com.susumunoda.kansha.ui.screen.auth.SignupScreen
 
 enum class UnauthenticatedScreen { LOGIN, SIGNUP }
-
-private const val SCREEN_TRANSITION_DURATION = 350
 
 @Composable
 fun UnauthenticatedNavigation(navController: NavHostController = rememberNavController()) {
@@ -24,24 +21,8 @@ fun UnauthenticatedNavigation(navController: NavHostController = rememberNavCont
         }
         composable(
             route = UnauthenticatedScreen.SIGNUP.name,
-            enterTransition = {
-                slideIntoContainer(
-                    towards = AnimatedContentTransitionScope.SlideDirection.Left,
-                    animationSpec = tween(
-                        durationMillis = SCREEN_TRANSITION_DURATION,
-                        easing = FastOutSlowInEasing
-                    )
-                )
-            },
-            exitTransition = {
-                slideOutOfContainer(
-                    towards = AnimatedContentTransitionScope.SlideDirection.Right,
-                    animationSpec = tween(
-                        durationMillis = SCREEN_TRANSITION_DURATION,
-                        easing = FastOutSlowInEasing
-                    )
-                )
-            }
+            enterTransition = { enterSlidingLeft() },
+            exitTransition = { exitSlidingRight() }
         ) {
             SignupScreen(navController)
         }
