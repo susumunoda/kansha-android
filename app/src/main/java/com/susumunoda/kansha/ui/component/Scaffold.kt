07b98343376c6aedb12e1ab5821fbfeeb8d0694c
Susumu Noda <susumunoda@gmail.com>
2023-08-31 11:50:36 -0700

    Expose more params for ScaffoldWithStatusBarInsets

--- app/src/main/java/com/susumunoda/kansha/ui/component/Scaffold.kt
+++ app/src/main/java/com/susumunoda/kansha/ui/component/Scaffold.kt
@@ -1,17 +1,41 @@
 package com.susumunoda.kansha.ui.component
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.ScaffoldDefaults
+import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
 @Composable
 fun ScaffoldWithStatusBarInsets(
+    modifier: Modifier = Modifier,
     topBar: @Composable () -> Unit = {},
+    bottomBar: @Composable () -> Unit = {},
+    snackbarHost: @Composable () -> Unit = {},
+    floatingActionButton: @Composable () -> Unit = {},
+    floatingActionButtonPosition: FabPosition = FabPosition.End,
+    containerColor: Color = MaterialTheme.colorScheme.background,
+    contentColor: Color = contentColorFor(containerColor),
+    contentWindowInsets: WindowInsets = ScaffoldDefaults.contentWindowInsets,
     content: @Composable () -> Unit
 ) {
-    Scaffold(topBar = topBar) { contentPadding ->
+    Scaffold(
+        modifier = modifier,
+        topBar = topBar,
+        bottomBar = bottomBar,
+        snackbarHost = snackbarHost,
+        floatingActionButton = floatingActionButton,
+        floatingActionButtonPosition = floatingActionButtonPosition,
+        containerColor = containerColor,
+        contentColor = contentColor,
+        contentWindowInsets = contentWindowInsets
+    ) { contentPadding ->
         Box(
             modifier = Modifier.padding(top = contentPadding.calculateTopPadding())
         ) {
