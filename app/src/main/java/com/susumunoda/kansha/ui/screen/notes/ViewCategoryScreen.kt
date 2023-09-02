d651461c4e37506f06253927e4dc9c02682851de
Susumu Noda <susumunoda@gmail.com>
2023-09-01 20:04:57 -0700

    Show circular progress indicator while bg image loads

--- app/src/main/java/com/susumunoda/kansha/ui/screen/notes/ViewCategoryScreen.kt
+++ app/src/main/java/com/susumunoda/kansha/ui/screen/notes/ViewCategoryScreen.kt
@@ -1,5 +1,6 @@
 package com.susumunoda.kansha.ui.screen.notes
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -14,6 +15,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.ArrowBack
 import androidx.compose.material3.Card
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -24,10 +26,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.blur
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -35,8 +39,12 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
-import coil.compose.AsyncImage
+import coil.compose.AsyncImagePainter
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
+import coil.size.Size
 import com.susumunoda.kansha.R
+import com.susumunoda.kansha.repository.note.Note
 import com.susumunoda.kansha.ui.component.ScaffoldWithStatusBarInsets
 import com.susumunoda.kansha.ui.mock.MockCategory
 import com.susumunoda.kansha.ui.mock.MockNote
@@ -45,7 +53,6 @@ import com.susumunoda.kansha.ui.mock.MockProvider
 private const val TRANSPARENT_BACKGROUND_ALPHA = 0.75f
 private val BACKGROUND_BLUR_RADIUS = 16.dp
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ViewCategoryScreen(
     navController: NavHostController,
@@ -57,13 +64,25 @@ fun ViewCategoryScreen(
         viewModel.fetchNotes(categoryId)
     }
 
-    Box(Modifier.fillMaxSize()) {
-        val uiState by viewModel.uiState.collectAsState()
-        val category = viewModel.getCategory(categoryId)
+    val uiState by viewModel.uiState.collectAsState()
+    val category = viewModel.getCategory(categoryId)
+    val hasBackgroundPhoto = category?.photoUrl?.isNotBlank() == true
+    val painter = if (hasBackgroundPhoto) {
+        rememberAsyncImagePainter(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(category?.photoUrl)
+                .size(Size.ORIGINAL)
+                .build()
+        )
+    } else null
+    val transparentBackgroundColor = MaterialTheme.colorScheme.surface.copy(
+        alpha = TRANSPARENT_BACKGROUND_ALPHA
+    )
 
-        if (category != null) {
-            AsyncImage(
-                model = category.photoUrl,
+    Box(Modifier.fillMaxSize()) {
+        if (painter != null) {
+            Image(
+                painter = painter,
                 contentDescription = null,
                 contentScale = ContentScale.Crop,
                 modifier = Modifier
@@ -71,46 +90,72 @@ fun ViewCategoryScreen(
                     .fillMaxSize()
             )
         }
-
-        val transparentBackgroundColor =
-            MaterialTheme.colorScheme.surface.copy(alpha = TRANSPARENT_BACKGROUND_ALPHA)
         ScaffoldWithStatusBarInsets(
             containerColor = Color.Transparent,
             topBar = {
-                CenterAlignedTopAppBar(
-                    title = { Text(categoryName) },
-                    navigationIcon = {
-                        IconButton(onClick = { navController.popBackStack() }) {
-                            Icon(
-                                Icons.Rounded.ArrowBack,
-                                contentDescription = stringResource(R.string.back_button_description)
-                            )
-                        }
-                    },
-                    colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
-                        containerColor = transparentBackgroundColor
-                    )
+                TopBar(
+                    title = categoryName,
+                    backgroundColor = transparentBackgroundColor,
+                    onBackPressed = { navController.popBackStack() }
                 )
             }
         ) {
-            val padding = dimensionResource(R.dimen.padding_medium)
-            LazyColumn(
-                contentPadding = PaddingValues(padding),
-                verticalArrangement = Arrangement.spacedBy(padding),
+            Box(
                 modifier = Modifier
                     .fillMaxSize()
                     .background(transparentBackgroundColor)
             ) {
-                items(uiState.notes) { note ->
-                    Card(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .height(dimensionResource(R.dimen.card_height))
-                    ) {
-                        Box(Modifier.padding(padding)) {
-                            Text(note.message)
-                        }
-                    }
+                val imageLoadingSucceeded = painter?.state is AsyncImagePainter.State.Success
+                val imageLoadingFailed = painter?.state is AsyncImagePainter.State.Error
+                if (!hasBackgroundPhoto || imageLoadingSucceeded || imageLoadingFailed) {
+                    NotesList(uiState.notes)
+                } else {
+                    CircularProgressIndicator(Modifier.align(Alignment.Center))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+private fun TopBar(
+    title: String,
+    backgroundColor: Color,
+    onBackPressed: () -> Unit
+) {
+    CenterAlignedTopAppBar(
+        title = { Text(title) },
+        navigationIcon = {
+            IconButton(onClick = onBackPressed) {
+                Icon(
+                    Icons.Rounded.ArrowBack,
+                    contentDescription = stringResource(R.string.back_button_description)
+                )
+            }
+        },
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+            containerColor = backgroundColor
+        )
+    )
+}
+
+@Composable
+private fun NotesList(notes: List<Note>, modifier: Modifier = Modifier) {
+    val padding = dimensionResource(R.dimen.padding_medium)
+    LazyColumn(
+        contentPadding = PaddingValues(padding),
+        verticalArrangement = Arrangement.spacedBy(padding),
+        modifier = modifier
+    ) {
+        items(notes) { note ->
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(dimensionResource(R.dimen.card_height))
+            ) {
+                Box(Modifier.padding(padding)) {
+                    Text(note.message)
                 }
             }
         }
