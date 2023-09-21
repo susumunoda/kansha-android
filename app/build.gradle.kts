2d4b3ac779320f63e7aaf1333218a74be1ef41c2
Susumu Noda <susumunoda@gmail.com>
2023-09-20 18:11:29 -0700

    Upgrade navigation-compose to 2.7.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,7 +80,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3:1.2.0-alpha05")
-    implementation("androidx.navigation:navigation-compose:2.7.0")
+    implementation("androidx.navigation:navigation-compose:2.7.2")
     implementation("com.github.susumunoda:compose-navigation:a6f8b1aeca")
     implementation("com.github.susumunoda:compose-animation:6f52281543")
     implementation("com.github.susumunoda:compose-material3-components:ee846b7f86")
